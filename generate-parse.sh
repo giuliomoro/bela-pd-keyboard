8e858cea90db9c5d1fb0eb68a646d7666b161471
Giulio Moro <giuliomoro@yahoo.it>
2023-03-20 21:39:43 +0000

    Printing in comment the code

--- generate-parse.sh
+++ generate-parse.sh
@@ -6,7 +6,9 @@ OUT_FILE=parse-input-events.h
 INPUT_EVENT_CODES=/usr/include/linux/input-event-codes.h
 # inspired from https://stackoverflow.com/a/2554421/2958741
 echo generating parse.h
-echo -en "struct parse_key {\n\tunsigned int code;\n\tchar ascii;\n};\n" > $OUT_FILE
+> $OUT_FILE
+echo -en "// DO NOT EDIT: File generated automatically by $(basename $0) from $INPUT_EVENT_CODES\n" >> $OUT_FILE
+echo -en "struct parse_key {\n\tunsigned int code;\n\tchar ascii;\n};\n" >> $OUT_FILE
 echo -en "struct parse_key keynames[] = {\n" >> $OUT_FILE
 
 # only letters and numbers and the values listed below are actually parsed. Add
@@ -27,20 +29,20 @@ pairs=( \
 
 > $TMP_FILE
 grep "#define KEY_*[A-Z0-9]\s\|#define KEY_*KP[0-9]\s"  $INPUT_EVENT_CODES|\
-	sed "s/#define KEY_\(KP\)*\([^\s\t]\+\)\s\+\([0-9]\+\)/\3, '\2'/" \
+	sed "s@#define KEY_\(KP\)*\([^\s\t]\+\)\s\+\([0-9]\+\)@\3, '\2' }, // KEY_\1\2@" \
 	>> $TMP_FILE
 
 for (( n=0; n < ${#pairs[@]}; n=$((n+2)) )); do
 	name=${pairs[$n]}
 	ascii=${pairs[$((n+1))]}
 	grep "#define KEY_KP$name\s\|#define KEY_$name\s" $INPUT_EVENT_CODES |\
-		sed "s@#define KEY_\(KP\)*$name\s\+\([0-9]\+\)@\2, '$ascii'@" \
+		sed "s@#define KEY_\(KP\)*$name\s\+\([0-9]\+\)@\2, '$ascii' }, // KEY_\1$name@" \
 		>> $TMP_FILE
 done
 
 cat $TMP_FILE |\
 	sort -n |\
-	sed "s/\(^.*$\)/{\1},/" \
+	sed "s/\(^.*$\)/{\1/" \
 	>> $OUT_FILE
 echo -en "};\n" \
        >> $OUT_FILE
